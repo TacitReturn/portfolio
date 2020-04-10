b711a9f6c303af7f36de1c64fccd4cb025492221
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-10 15:10:16 -0400

    made changes to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GR Portfolio
                 </div>
 
                 <div class="links">
