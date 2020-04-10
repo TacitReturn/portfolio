5ca92e70a1fda9f6e3af5902dd5013253c9401d9
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-10 18:12:14 -0400

    made changes to resume.blade.php

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="jumbotron">
-    <h2 class="display-4">Hello, there!</h2>
+    <h2 class="display-4">Resume!</h2>
     <p class="lead">Welcome to my portfolio, you'll find my projects, resume and blog here.</p>
     <a class="btn btn-primary btn-lg" href="#" role="button">Contact</a>
 </div>
