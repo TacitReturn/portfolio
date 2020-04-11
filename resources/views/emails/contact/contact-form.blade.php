1d1826717e0446b51c6d6a08f283a462e02df879
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-11 02:01:31 -0400

    made changes to contact-form

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -2,12 +2,12 @@
     # Thanks for contacting me,
 
     I'll try my best to get back to you in 2-3 business days (excluding Saturday).
-    <br/>
+
     If you'd like to view some of the code I'm currently working on, you can get a more in dept
     look on my GitHub profile, I update projects there more frequently before they're pushed to
     my portfolio.
+
     @component('mail::button', ['url' => 'https://github.com/TacitReturn'])
         GitHub Profile
-
     @endcomponent
 @endcomponent
