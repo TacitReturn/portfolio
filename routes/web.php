0f945579efb4ed4d1075369e3e7744788361deee
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-11 00:47:51 -0400

    made ContactFormController and set up routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@index')->name('index');
-Route::get('/resume', 'PagesController@resume')->name('resume');
-Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::get('resume', 'PagesController@resume')->name('resume');
+Route::get('contact', 'ContactFormController@create')->name('contact-me');
 
 Auth::routes();
 
