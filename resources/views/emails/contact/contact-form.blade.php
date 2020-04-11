1d4f14cc7230000c8cbed29420d63edc20b446ef
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-11 01:52:11 -0400

    made changes to email template

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -1,12 +1,23 @@
 @component('mail::message')
-# Introduction
+    # Thanks for contacting me,
 
-The body of your message.
+    I'll try my best to get back to you in 2-3 business days (excluding Saturday).
+    <br/>
+    If you'd like to view some of the code I'm currently working on, you can get a more in dept
+    look on my GitHub profile, I update projects there more frequently before they're pushed to
+    my portfolio.
+    @component('mail::button', ['url' => 'https://github.com/TacitReturn'])
+        GitHub Profile
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
+    @endcomponent@component('mail::button', ['url' => 'https://www.linkedin.com/in/glenn-rudge/'])
+        LinkedIn Profile
+    @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    Glenn Rudge
+    GlennRudge@outlook.com
+    Miami, Florida 33179
+    1-305-904-1725
+    Hephaestus-Solutions.com
+    {{ config('app.name') }}
 @endcomponent
