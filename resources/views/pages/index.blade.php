31e41bcbd157ea10f8fda4f365fba0741cfdb040
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-14 17:49:43 -0400

    made some changes to index.blade

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <div class="jumbotron">
     <h2 class="display-4">Hello, there!</h2>
-    <p class="lead">Welcome to my portfolio, you'll find my projects, resume and blog here.</p>
+    <p class="lead">Welcome to my portfolio, you'll find my projects, resume and blog here --
+    login and/or register if you're interested in IoT and tutorial material from my blog.</p>
     <a class="btn btn-primary btn-lg" href="{{route('contact-me')}}" role="button">Contact</a>
     <p class="lead">If you're interested in more recent projects you can check out my GitHub repo
         <span><a target="_blank" href="https://github.com/TacitReturn"> here.</a></span>
