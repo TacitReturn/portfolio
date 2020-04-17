d595571159f67a57b721b152ebcad27d30ce966d
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-17 19:09:49 -0400

    made changes to PostsController again

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,8 @@ class PostsController extends Controller
         //
         $posts = Post::all();
 
-        return view('posts.index')->with('posts', $posts);
+//        return view('posts.index')->with('posts', $posts);
+        dd($posts);
     }
 
     /**
@@ -33,7 +34,7 @@ class PostsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -44,7 +45,7 @@ class PostsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Post  $post
+     * @param \App\Post $post
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post)
@@ -55,7 +56,7 @@ class PostsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Post  $post
+     * @param \App\Post $post
      * @return \Illuminate\Http\Response
      */
     public function edit(Post $post)
@@ -66,8 +67,8 @@ class PostsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Post  $post
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Post $post
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Post $post)
@@ -78,7 +79,7 @@ class PostsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Post  $post
+     * @param \App\Post $post
      * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
