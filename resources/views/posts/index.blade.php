e1b41386398f5f0915c1af334e70193f41205fc6
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-17 19:25:04 -0400

    styled index.blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Posts</h1>
 
     @if(count($posts) > 0)
-        <ul>
-            @foreach($posts as $post)
-                <li>
-                    {{$post->title}}
-                </li>
-            @endforeach
-        </ul>
-
+        @foreach($posts as $post)
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{$post->title}}</h5>
+                    <small class="card-text">{{$post->created_at }}</small>
+                    <p class="card-text">{{$post->body }}</p>
+                    <a class="card-link">Read More..</a>
+                    {{--                <a class="card-link">Another link</a>--}}
+                </div>
+            </div>
+        @endforeach
     @endif
 
 @endsection
