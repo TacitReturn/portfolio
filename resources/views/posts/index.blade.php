ca1bb1093556eecd50ba80522f3826b6e42f9083
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-17 19:18:51 -0400

    made more changes to index.blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,17 +3,14 @@
 @section('content')
 
     @if(count($posts) > 0)
-        @foreach($posts as $post)
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">{{$post->title}}</h5>
-                    <p class="card-text">{{$post->created_at }}</p>
-                    <p class="card-text">{{$post->body }}</p>
-                    <a class="card-link">Read More..</a>
-                    {{--                <a class="card-link">Another link</a>--}}
-                </div>
-            </div>
-        @endforeach
+        <ul>
+            @foreach($posts as $post)
+                <li>
+                    {{$post->title}}
+                </li>
+            @endforeach
+        </ul>
+
     @endif
 
 @endsection
