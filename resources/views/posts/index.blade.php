4d6180a52f1f838dec9a08c8065b6bcf16c3eb97
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-17 20:24:32 -0400

    made y axis changes on index.blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
 
     @if(count($posts) > 0)
         @foreach($posts as $post)
-            <div class="card my-5 mx-5">
+            <div class="card my-3">
                 <div class="card-body">
                     <h5 class="card-title">{{$post->title}}</h5>
                     <small class="card-text">Posted On: {{$post->created_at }}</small>
