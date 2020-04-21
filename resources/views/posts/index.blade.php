511e9b407a5ad541e439555730f91b3589735254
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-21 18:29:32 -0400

    made aesthetic changes to index and show page for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,9 +9,14 @@
                     <div class="card-body">
                         <h5 class="card-title">{{$post->title}}</h5>
                         <small class="card-text">
-                            Posted on {{$post->created_at->format('jS F Y h:i:s A') }}
-                            <br />
-                            by {{{ isset(Auth::user()->name) ? Auth::user()->name : null }}}
+                            Posted on
+                            <span class="badge badge-pill badge-primary">
+                    {{$post->created_at->format('jS F Y h:i:s A') }}
+                </span>
+                            by
+                            <span class="badge badge-pill badge-primary">
+                    {{ isset(Auth::user()->name) ? Auth::user()->name : null }}
+                </span>
                         </small>
                         <br/>
                         <p class="card-text">{{$post->body }}</p>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,9 +5,15 @@
         <div class="card-body">
             <h5 class="card-title">{{$post->title}}</h5>
             <small class="card-text">
-                Posted on {{$post->created_at->format('jS F Y h:i:s A') }}
-                <br />
-                by {{ isset(Auth::user()->name) ? Auth::user()->name : null }}
+                Posted on
+                <span class="badge badge-pill badge-primary">
+                    {{$post->created_at->format('jS F Y h:i:s A') }}
+                </span>
+                by
+                <span class="badge badge-pill badge-primary">
+                    {{ isset(Auth::user()->name) ? Auth::user()->name : null }}
+                </span>
+
             </small>
             <p class="card-text">{{$post->body }}</p>
             {{--                <a class="card-link">Another link</a>--}}
