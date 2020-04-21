bbda627c1d696f3c8f83956294e500b276143f76
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-21 10:12:21 -0400

    made changes to show.blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,10 @@
     <div class="card my-3">
         <div class="card-body">
             <h5 class="card-title">{{$post->title}}</h5>
-            <small class="card-text">Posted On: {{$post->created_at }}</small>
+            <small class="card-text">
+                Posted on {{$post->created_at->format('jS F Y h:i:s A') }}
+                by {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}
+            </small>
             <p class="card-text">{{$post->body }}</p>
             {{--                <a class="card-link">Another link</a>--}}
         </div>
