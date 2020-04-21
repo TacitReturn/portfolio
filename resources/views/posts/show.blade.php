918d40e286cb782fa1a5fb31e076c64ec98aba9e
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-21 18:34:53 -0400

    more aethetic changes to posts.index and posts.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,11 +5,11 @@
         <div class="card-body">
             <h5 class="card-title">{{$post->title}}</h5>
             <small class="card-text">
-                Posted on
+                Posted on:
                 <span class="badge badge-pill badge-primary">
                     {{$post->created_at->format('jS F Y h:i:s A') }}
                 </span>
-                by
+                by:
                 <span class="badge badge-pill badge-primary">
                     {{ isset(Auth::user()->name) ? Auth::user()->name : 'Must be signed in.' }}
                 </span>
