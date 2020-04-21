6f8c85eab5d619cba77d8841c6dc637b764a0359
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-21 10:02:29 -0400

    made some changes index.blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,8 @@
                 <div class="card-body">
                     <h5 class="card-title">{{$post->title}}</h5>
                     <small class="card-text">Posted On: {{$post->created_at }}</small>
-                    <small class="card-text">Created By: {{ Auth::user()->username }}</small>
+                    <small class="card-text">Created
+                        By: {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</small>
                     <p class="card-text">{{$post->body }}</p>
                     <a href="/posts/{{$post->id}}" class="card-link">Read More..</a>
                     {{--                <a class="card-link">Another link</a>--}}
