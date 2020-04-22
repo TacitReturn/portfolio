9fb05331b3f92529cb61842d18308283dc5718f9
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-22 03:51:19 -0400

    made changes to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use http\Client\Curl\User;
 use Illuminate\Http\Request;
 use App\Post;
+use App\User;
+
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
