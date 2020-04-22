59dc4930b3e920855313d9c72119b2af860d92a0
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-22 04:55:53 -0400

    made changes to Post, User model and HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use http\Client\Curl\User;
 use Illuminate\Http\Request;
 use App\Post;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -26,9 +27,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user_id = Auth::user()->id;
+        $user_id = auth()->user()->id;
         $user = User::find($user_id);
-        return view('home')->with('posts', $user->posts);
-
+        return view('dashboard')->with('posts', $user->posts);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    public function user() {
+    public function user(){
         return $this->belongsTo('App\User');
     }
 }
