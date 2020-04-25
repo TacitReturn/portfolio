9199893238d55a4e2cea56f6844222fbda8cedf0
Glenn Rudge <glenn@hephaestus-solutions.com>
2020-04-25 02:23:53 -0400

    working on update method in PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -101,7 +101,7 @@ class PostsController extends Controller
         $post->body = $request->input('body');
         $post->save();
 
-        return redirect('posts');
+        return redirect('/posts')->with('success', 'Post Updated');
 
     }
 
