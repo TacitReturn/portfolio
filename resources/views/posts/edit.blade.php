242cabba9c09ee45fd311747d4c98b474b19a044
TacitReturn <glennrudge@outlook.com>
2020-04-29 03:03:02 -0400

    made changes to edit and show views in PostsController

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,59 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-    <!--Section: Contact v.2-->
-    <section class="mb-4">
+    <div class="container">
 
-        <!--Section heading-->
-        <h2 class="h1-responsive font-weight-bold text-center my-4">Update Post</h2>
-        <div class="row">
+        <!--Section: Contact v.2-->
+        <section class="mb-4">
 
-            <!--Grid column-->
-            <div class="col-md-9 mb-md-0 mb-5">
-                <form id="contact-form" name="contact-form" action="{{route('posts.update', $post->id)}}" method="POST">
-                @csrf
-                @method('PUT')
-                <!--Grid row-->
-                    <div class="row">
+            <!--Section heading-->
+            <h2 class="h1-responsive font-weight-bold text-center my-4">Update Post</h2>
+            <div class="row">
 
-                        <!--Grid column-->
-                        <div class="col-md-6">
-                            <div class="md-form mb-0">
-                                <input type="text" id="title" name="title" class="form-control"
-                                       value="{{$post->title}}">
-                                <label for="title" class="">Title</label>
+                <!--Grid column-->
+                <div class="col-md-9 mb-md-0 mb-5">
+                    <form id="contact-form" name="contact-form" action="{{route('posts.update', $post->id)}}"
+                          method="POST">
+                    @csrf
+                    @method('PUT')
+                    <!--Grid row-->
+                        <div class="row">
+
+                            <!--Grid column-->
+                            <div class="col-md-6">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="title" name="title" class="form-control"
+                                           value="{{$post->title}}">
+                                    <label for="title" class="">Title</label>
+                                </div>
                             </div>
+
                         </div>
 
-                    </div>
+                        <div class="row">
 
-                    <div class="row">
+                            <!--Grid column-->
+                            <div class="col-md-12">
 
-                        <!--Grid column-->
-                        <div class="col-md-12">
-
-                            <div class="md-form">
+                                <div class="md-form">
                             <textarea type="text" id="body" name="body" rows="2"
                                       class="form-control md-textarea">{{$post->body}}
                             </textarea>
-                                <label for="message">Post Content</label>
+                                    <label for="message">Post Content</label>
+                                </div>
+
                             </div>
-
                         </div>
-                    </div>
-                    <!--Grid row-->
-                    <div class="btn-group btn-group-lg">
-                        <button type="submit" class="btn btn-primary">
-                            Update Post
-                        </button>
-                    </div>
-                </form>
+                        <!--Grid row-->
+                        <div class="btn-group btn-group-lg">
+                            <button type="submit" class="btn btn-primary">
+                                Update Post
+                            </button>
+                        </div>
+                    </form>
 
-                <div class="status"></div>
+                    <div class="status"></div>
+                </div>
+
             </div>
 
-        </div>
-
-    </section>
-    <!--Section: Contact v.2-->
+        </section>
+        <!--Section: Contact v.2-->
+    </div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card my-3">
-        <div class="card-body">
-            <h5 class="card-title">{{$post->title}}</h5>
-            <small class="card-text">
-                Posted on:
-                <span class="badge badge-pill badge-primary">
+    <div class="container">
+        <div class="card my-3">
+            <div class="card-body">
+                <h5 class="card-title">{{$post->title}}</h5>
+                <small class="card-text">
+                    Posted on:
+                    <span class="badge badge-pill badge-primary">
                     {{$post->created_at->format('jS F Y h:i:s A') }}
                 </span>
-                by:
-                <span class="badge badge-pill badge-primary">
+                    by:
+                    <span class="badge badge-pill badge-primary">
                     {{$post->user->name}}
                 </span>
 
-            </small>
-            <p class="card-text">{{$post->body }}</p>
-            <div class="text-center text-md-left">
-                @if(!Auth::guest())
-                    @if(Auth::user()->id == $post->user_id)
-                        <a href="/posts/{{$post->id}}/edit" class="card-link btn btn-primary">Edit Post</a>
+                </small>
+                <p class="card-text">{{$post->body }}</p>
+                <div class="text-center text-md-left">
+                    @if(!Auth::guest())
+                        @if(Auth::user()->id == $post->user_id)
+                            <a href="/posts/{{$post->id}}/edit" class="card-link btn btn-primary">Edit Post</a>
 
-                        <form action="/posts/{{$post->id}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="card-link btn btn-danger">Delete Post</button>
-                        </form>
+                            <form action="/posts/{{$post->id}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="card-link btn btn-danger">Delete Post</button>
+                            </form>
+                        @endif
                     @endif
-                @endif
+                </div>
             </div>
         </div>
     </div>
+
 @endsection
